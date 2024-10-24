6031361b81a398f8dc828d48798e34ac27a9c3e4
demondcn <guerrero70407@gmail.com>
2024-10-24 15:49:19 -0500

    api agregacion3

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,7 +30,7 @@ const Inicio = () => {
 
   const onSubmitRegister = async (data) => {
     try {
-      const res = await fetch('/api/auth/registro', {
+      const res = await fetch('Server/api/auth/registro', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
@@ -78,12 +78,12 @@ const Inicio = () => {
     <div
       className="min-h-screen flex items-center justify-center p-4"
       style={{
-        background: 'linear-gradient(-45deg, #4E9419, #2C5234)',
+        background: 'linear-gradient(-45deg, #FFD700 , #FFFACD)',
         backgroundSize: '400% 400%',
         animation: 'gradientAnimation 15s ease infinite',
       }}
     >
-      <div className="bg-[#21313C] rounded-2xl shadow-2xl flex w-full max-w-4xl overflow-hidden">
+      <div className="bg-[#A8D5BA] rounded-2xl shadow-2xl flex w-full max-w-4xl overflow-hidden">
         <motion.div
           className="w-1/2 p-8 relative"
           initial={{ x: isLogin ? 0 : '100%' }}
@@ -91,17 +91,17 @@ const Inicio = () => {
           transition={{ duration: 0.6 }}
         >
           <div className="absolute top-5 left-1/2 transform -translate-x-1/2">
-            <Image
+            {/* <Image
               src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/934998f8-8c24-41fd-bf88-93a6fc55d9fc-8exqPk7JrayPcJsRekohG0YpSGQP9W.jpg"
               alt="Empres 360 Pro Logo"
               width={180}
               height={75}
               className="object-contain"
-            />
+            /> */}
           </div>
 
           <div className="mt-24">
-            <h2 className="text-3xl font-bold text-green-500 mb-6">
+            <h2 className="text-3xl font-bold text-yellow-500 mb-6">
               {isLogin ? 'Iniciar Sesión' : 'Crear Cuenta'}
             </h2>
             <form onSubmit={isLogin ? handleSubmitLogin(onSubmitLogin) : handleSubmitRegister(onSubmitRegister)} className="space-y-4">
@@ -123,7 +123,7 @@ const Inicio = () => {
                   type="email"
                   placeholder="Correo"
                   className="w-full p-2 pl-10 rounded-xl border bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
-                  {...(isLogin ? registerLogin : registerRegister)("email", { 
+                  {...(isLogin ? registerLogin : registerRegister)("email", {
                     required: "El correo es requerido",
                     pattern: {
                       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
@@ -131,7 +131,7 @@ const Inicio = () => {
                     }
                   })}
                 />
-                {(isLogin ? errorsLogin.email : errorsRegister.email) && 
+                {(isLogin ? errorsLogin.email : errorsRegister.email) &&
                   <p className="text-red-500 text-xs mt-1">{isLogin ? errorsLogin.email.message : errorsRegister.email.message}</p>}
               </div>
               <div className="relative">
@@ -140,7 +140,7 @@ const Inicio = () => {
                   type={showPassword ? "text" : "password"}
                   placeholder="Contraseña"
                   className="w-full p-2 pl-10 pr-10 rounded-xl border bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
-                  {...(isLogin ? registerLogin : registerRegister)("password", { 
+                  {...(isLogin ? registerLogin : registerRegister)("password", {
                     required: "La contraseña es requerida",
                     minLength: {
                       value: 6,
@@ -155,7 +155,7 @@ const Inicio = () => {
                 >
                   {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                 </button>
-                {(isLogin ? errorsLogin.password : errorsRegister.password) && 
+                {(isLogin ? errorsLogin.password : errorsRegister.password) &&
                   <p className="text-red-500 text-xs mt-1">{isLogin ? errorsLogin.password.message : errorsRegister.password.message}</p>}
               </div>
               {isLogin && (
@@ -163,14 +163,14 @@ const Inicio = () => {
                   ¿Perdiste tu contraseña?
                 </a>
               )}
-              <button className="w-full bg-green-500 text-white py-2 rounded-xl hover:bg-green-600 transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
+              <button className="w-full bg-yellow-500 text-white py-2 rounded-xl hover:bg-yellow-600 transition duration-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50">
                 {isLogin ? 'Iniciar sesión' : 'Registrarse'}
               </button>
             </form>
           </div>
         </motion.div>
         <motion.div
-          className="w-1/2 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-16 px-12 relative overflow-hidden"
+          className="w-1/2 bg-yellow-500 text-white rounded-tr-2xl rounded-br-2xl py-16 px-12 relative overflow-hidden"
           initial={{ x: isLogin ? 0 : '-100%' }}
           animate={{ x: isLogin ? 0 : '-100%' }}
           transition={{ duration: 0.6 }}
@@ -181,11 +181,12 @@ const Inicio = () => {
           <div className="border-2 w-10 border-white inline-block mb-4 relative z-10"></div>
           <p className="mb-10 relative z-10 text-lg">
             {isLogin
-              ? 'Introduce tus datos personales y comienza a llevar tu empresa al siguiente nivel.'
-              : 'Impulse el rendimiento de su empresa. Regístrate hoy y aprovecha esta oportunidad para llevar tu empresa al siguiente nivel.'}
+              ? 'Introduce tus datos personales y comienza tu camino hacia el bienestar integral.'
+              : 'Mejora tu equilibrio emocional y bienestar. Regístrate hoy y aprovecha esta oportunidad para transformar tu vida holísticamente.'}
+
           </p>
           <button
-            className="border-2 border-white rounded-xl px-12 py-3 inline-block font-semibold hover:bg-white hover:text-green-500 transition duration-300 relative z-10 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
+            className="border-2 border-white rounded-xl px-12 py-3 inline-block font-semibold hover:bg-white hover:text-yellow-500 transition duration-300 relative z-10 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
             onClick={() => setIsLogin(!isLogin)}
           >
             {isLogin ? 'Registrarse' : 'Iniciar sesión'}
