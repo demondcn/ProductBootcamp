98e2e99938fe3be9808261990152cf76a5118415
demondcn <guerrero70407@gmail.com>
2024-10-23 21:32:40 -0500

    pruebo

--- src/app/InicioSeccion/Sub1/page.jsx
+++ src/app/InicioSeccion/Sub1/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 import React from 'react';
-import Sub1 from '../../../components/Sub2';
+import Sub2 from '@/components/Sub2';
 
 const Pasarel = () => {
   return (
-      <Sub1 />
+      <Sub2 />
   );
 };
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@
 "use client";
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
+import { SessionProvider } from 'next-auth/react';
 import "./globals.css"
 
 const fontHeading = Inter({
