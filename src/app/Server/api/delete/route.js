a4f779223785e16b41303adb2ba6a7b835bcc373
demondcn <guerrero70407@gmail.com>
2024-10-23 23:10:01 -0500

    pruebesis

--- src/app/Server/api/delete/route.js
+++ src/app/Server/api/delete/route.js
@@ -1,60 +1,60 @@
 
 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-// Inicializa Prisma Client
-const prisma = new PrismaClient();
+// // Inicializa Prisma Client
+// const prisma = new PrismaClient();
 
-export async function DELETE(request) {
-  try {
-    // Obtener el cuerpo de la solicitud
-    const { id } = await request.json();
+// export async function DELETE(request) {
+//   try {
+//     // Obtener el cuerpo de la solicitud
+//     const { id } = await request.json();
 
-    if (!id) {
-      return new Response(JSON.stringify({ error: 'ID no proporcionado' }), { status: 400, headers: { 'Content-Type': 'application/json' } });
-    }
+//     if (!id) {
+//       return new Response(JSON.stringify({ error: 'ID no proporcionado' }), { status: 400, headers: { 'Content-Type': 'application/json' } });
+//     }
 
-    // Eliminar el usuario y todos los registros asociados en una transacción
-    await prisma.$transaction(async (prisma) => {
-      // Eliminar las pruebas asociadas a los diagnósticos del usuario
-      await prisma.test.deleteMany({
-        where: {
-          diagnosis: {
-            userId: id
-          }
-        }
-      });
+//     // Eliminar el usuario y todos los registros asociados en una transacción
+//     await prisma.$transaction(async (prisma) => {
+//       // Eliminar las pruebas asociadas a los diagnósticos del usuario
+//       await prisma.test.deleteMany({
+//         where: {
+//           diagnosis: {
+//             userId: id
+//           }
+//         }
+//       });
 
-      // Eliminar los diagnósticos del usuario
-      await prisma.diagnosis.deleteMany({
-        where: {
-          userId: id
-        }
-      });
+//       // Eliminar los diagnósticos del usuario
+//       await prisma.diagnosis.deleteMany({
+//         where: {
+//           userId: id
+//         }
+//       });
 
-      // Eliminar las empresas del usuario
-      await prisma.empresa.deleteMany({
-        where: {
-          userId: id
-        }
-      });
+//       // Eliminar las empresas del usuario
+//       await prisma.empresa.deleteMany({
+//         where: {
+//           userId: id
+//         }
+//       });
 
-      // Eliminar los registros de restablecimiento de contraseña del usuario
-      await prisma.passwordReset.deleteMany({
-        where: {
-          userId: id
-        }
-      });
+//       // Eliminar los registros de restablecimiento de contraseña del usuario
+//       await prisma.passwordReset.deleteMany({
+//         where: {
+//           userId: id
+//         }
+//       });
 
-      // Finalmente, eliminar el usuario
-      await prisma.user.delete({
-        where: { id }
-      });
-    });
+//       // Finalmente, eliminar el usuario
+//       await prisma.user.delete({
+//         where: { id }
+//       });
+//     });
 
-    return new Response(JSON.stringify({ message: 'Usuario y registros asociados eliminados' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
-  } catch (error) {
-    console.error('Error al eliminar el usuario:', error);
-    return new Response(JSON.stringify({ error: 'Error al eliminar el usuario' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
-  }
-}
+//     return new Response(JSON.stringify({ message: 'Usuario y registros asociados eliminados' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+//   } catch (error) {
+//     console.error('Error al eliminar el usuario:', error);
+//     return new Response(JSON.stringify({ error: 'Error al eliminar el usuario' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+//   }
+// }
--- src/app/Server/api/uptate/route.js
+++ src/app/Server/api/uptate/route.js
@@ -1,28 +1,28 @@
-// app/api/uptateInfoUsers/route.js
+// // app/api/uptateInfoUsers/route.js
 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function PUT(request) {
-  try {
-    // Obtener el cuerpo de la solicitud
-    const {data, id} = await request.json();
-    const {name, email } = data;
+// export async function PUT(request) {
+//   try {
+//     // Obtener el cuerpo de la solicitud
+//     const {data, id} = await request.json();
+//     const {name, email } = data;
 
-    if (!id || !name || !email) {
-      return new Response(JSON.stringify({ error: 'Faltan datos requeridos' }), { status: 400, headers: { 'Content-Type': 'application/json' } });
-    }
+//     if (!id || !name || !email) {
+//       return new Response(JSON.stringify({ error: 'Faltan datos requeridos' }), { status: 400, headers: { 'Content-Type': 'application/json' } });
+//     }
 
-    // Actualizar la información del usuario
-    const updatedUser = await prisma.user.update({
-      where: { id },
-      data: { name, email },
-    });
+//     // Actualizar la información del usuario
+//     const updatedUser = await prisma.user.update({
+//       where: { id },
+//       data: { name, email },
+//     });
 
-    return new Response(JSON.stringify(updatedUser), { status: 200, headers: { 'Content-Type': 'application/json' } });
-  } catch (error) {
-    console.error('Error al actualizar el usuario:', error);
-    return new Response(JSON.stringify({ error: 'Error al actualizar el usuario' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
-  }
-}
+//     return new Response(JSON.stringify(updatedUser), { status: 200, headers: { 'Content-Type': 'application/json' } });
+//   } catch (error) {
+//     console.error('Error al actualizar el usuario:', error);
+//     return new Response(JSON.stringify({ error: 'Error al actualizar el usuario' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+//   }
+// }
